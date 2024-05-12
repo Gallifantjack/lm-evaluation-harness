59c7b7b0a504c9caacd5ffcb68447a52e04e9995
gallifantjack <gallifantjack@googlemail.com>
2024-05-12 18:00:24 -0400

    1Mill drug list + model iters

--- onBrand_run.sh
+++ onBrand_run.sh
@@ -4,11 +4,21 @@
 config_files=(
     "lm_eval/tasks/medmcqa/medmcqa.yaml"
     "lm_eval/tasks/medqa/medqa.yaml"
-    "lm_eval/tasks/mmlu/default/_default_template_yaml"
+    "lm_eval/tasks/mmlu/default/_default_template.yaml"
     "lm_eval/tasks/pubmedqa/pubmedqa.yaml"
     "lm_eval/tasks/usmle_sa_step1/usmle_sa_step1.yaml"
     "lm_eval/tasks/usmle_sa_step2/usmle_sa_step2.yaml"
     "lm_eval/tasks/usmle_sa_step3/usmle_sa_step3.yaml"
+)
+
+# List of models to evaluate
+models=(
+    "unsloth/gemma-7b"
+    "mistralai/Mistral-7B-v0.1"
+    "medalpaca/medalpaca-7b"
+    "BioMistral/BioMistral-7B"
+    "openai-community/gpt2-xl"
+    "EleutherAI/gpt-neo-2.7B"
 )
 
 # List of keyword_replace settings to apply
@@ -27,20 +37,29 @@ update_config() {
     done
 }
 
-# Loop over each keyword_replace setting
-for replace in "${keyword_replacements[@]}"; do
-    echo "Setting keyword_replace to $replace"
-    update_config $replace
+# Loop over each model and keyword_replace setting
+for model in "${models[@]}"; do
+    model_alias=$(echo $model | tr '/' '-')
+    for replace in "${keyword_replacements[@]}"; do
+        echo "Setting keyword_replace to $replace for model $model"
+        update_config $replace
 
-    # Run the evaluation command for the onBrand group task
-    echo "Running evaluation for keyword_replace: $replace"
-    python lm_eval --model hf \
-                   --model_args pretrained=EleutherAI/pythia-160m,revision=step100000,dtype="float" \
-                   --tasks onBrand \
-                   --device cuda:0 \
-                   --batch_size auto:64 \
-                   --output_path lm_eval/tasks/benchmarks/onBrand/results/ \
-                   --keyword_replace $replace
+        # Define output path to include model alias
+        output_path="lm_eval/tasks/benchmarks/onBrand/results/$model_alias/$replace"
+
+        # Create output directory if it does not exist
+        mkdir -p $output_path
+
+        # Run the evaluation command for the onBrand group task
+        echo "Running evaluation for model $model with keyword_replace: $replace"
+        python lm_eval --model hf \
+                       --model_args pretrained=$model \
+                       --tasks onBrand \
+                       --device cuda:0 \
+                       --batch_size auto:64 \
+                       --output_path $output_path \
+                       --keyword_replace $replace
+    done
 done
 
 # Restore the original configuration files
@@ -49,6 +68,3 @@ for file in "${config_files[@]}"; do
 done
 
 echo "All evaluations completed."
-
-
-
