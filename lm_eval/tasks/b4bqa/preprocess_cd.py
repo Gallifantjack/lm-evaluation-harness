e7b4ac6f0fdb8c3ae8bdc0f4cac3201ec6340d8b
shan <shawnchen23@gmail.com>
2024-06-03 17:34:15 -0400

    cancer_drug_mcqa, b4b_med_mcqa done

--- lm_eval/tasks/b4bqa/preprocess_cd.py
+++ lm_eval/tasks/b4bqa/preprocess_cd.py
@@ -1,6 +1,8 @@
+import ast
+
 def process_cd(doc) -> str:
     options = ""
-    for k, v in doc["choices"].items():
+    for k, v in ast.literal_eval(doc["choices"]).items():
         if v is not None:
             options += k + ". " + v + "\n"
     return doc["input"].strip() + "\n" + options + "Answer:"
---
+++ run_b4b_mingye.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Ensure the Hugging Face token is set as an environment variable
+if [ -z "$HF_TOKEN" ]; then
+    echo "Hugging Face token not found. Please set the HF_TOKEN environment variable by running 'export HF_TOKEN=your_token_here'."
+    exit 1
+fi
+
+# Login to Hugging Face
+echo $HF_TOKEN | huggingface-cli login --token
+
+# Define the base directory (use an absolute path if possible)
+# base_dir="/home/legionjgally/Desktop/mit/lm-evaluation-harness"
+base_dir="/home/jgally/mit/lm-evaluation-harness"
+python_env="/home/jgally/miniconda3/envs/b4b/bin/python"
+
+# List of models to evaluate
+models=(
+    # "microsoft/phi-1"
+    # "microsoft/phi-1_5"
+    # "microsoft/phi-2"
+    # "microsoft/Phi-3-mini-4k-instruct"
+    # # "meta-llama/Llama-2-7b-hf"
+    # # "meta-llama/Meta-Llama-3-8B"
+    # "mistralai/Mistral-7B-v0.1"
+    # "Qwen/Qwen1.5-7B"
+    # "medalpaca/medalpaca-7b"
+    "mistralai/Mixtral-8x7B-v0.1"
+    "mistralai/Mixtral-8x22B-v0.1"
+    "meta-llama/Llama-2-70B-hf"
+    "meta-llama/Llama-3-70B-hf"
+    "Qwen/Qwen1.5-72B"
+)
+
+# Loop over each model
+for model in "${models[@]}"; do
+    # Create a model alias by replacing '/' with '-'
+    model_alias=$(echo $model | tr '/' '-')
+
+    # Define output path to include model alias and ensure it exists with correct permissions
+    output_path="$base_dir/results/b4b"
+    mkdir -p $output_path
+    chmod -R u+rwx $output_path
+
+    # Define the results file name
+    results_file="$output_path/${model_alias}_results.json"
+
+    # Run the evaluation command for the onBrand group task
+    echo "Running evaluation for model $model"
+    $python_env lm_eval --model hf,load_in_4bit=True\
+                    --model_args pretrained=$model \
+                    --tasks b4b \
+                    --device cuda:0 \
+                    --batch_size auto:64 \
+                    --output_path $results_file
+done
+
+echo "All evaluations completed."
+
+# List for future ref
+# viii.    mistralai/Mistral-7B-vO.1
+# ix. mistralai/Mixtral-8x22B-v0.1
+# x. Llama-2-7B-hf
+# xi. Llama-2-70B-hf
+# xii. Llama-3-8B-hf
+# xiii. Llama-3-70B-hf
+# xiv. Qwen1.5-7b
+# xv. Qwen/Qwen1.5-72B
+# xvi. Qwen1.5-110b
+# XVII. GPT-40
+# xviii. GPT4-turbo
+# xix. Claude-3.0-haiku
+# xx. Claude-3.0-opus
+# xxi. Claude-3.0-sonnet
